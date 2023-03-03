e129ef65455eb6ab7f1cc20d7fcc51e36df66d01
Leonardo <leonardotandelicw@outlook.com>
2023-03-03 15:10:21 -0300

    create component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import { Link } from '@mui/material'
+
+export default function Footer() {
+  return (
+    <>
+<Typography> © 2023 - Todos os direitos reservados.</Typography>
+<Link>APIs Públicas</Link>
+</>
+  )
+}
---
+++ src/components/FooterInfo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+export default function FooterInfo() {
+  return (
+ <Typography>   Os resultados exibidos nessa página são apenas simulações, e não possuem nenhum valor legal. </Typography>
+  )
+}
